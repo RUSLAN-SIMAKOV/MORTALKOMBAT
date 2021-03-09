bcd0d51a68e78ccbba173eb0759fc5c097c1d9b5
RUSLAN SIMAKOV <ua667766706@gmail.com>
2021-03-09 23:01:17 +0200

    Refactoring autowired resttemplate

--- mortalkombatservice/src/main/java/ruslan/simakov/mortalkombatservice/MortalkombatserviceApplication.java
+++ mortalkombatservice/src/main/java/ruslan/simakov/mortalkombatservice/MortalkombatserviceApplication.java
@@ -2,10 +2,17 @@ package ruslan.simakov.mortalkombatservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MortalkombatserviceApplication {
 
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+    
     public static void main(String[] args) {
         SpringApplication.run(MortalkombatserviceApplication.class, args);
     }
--- mortalkombatservice/src/main/java/ruslan/simakov/mortalkombatservice/controller/impl/MortalKombatControllerImpl.java
+++ mortalkombatservice/src/main/java/ruslan/simakov/mortalkombatservice/controller/impl/MortalKombatControllerImpl.java
@@ -1,5 +1,6 @@
 package ruslan.simakov.mortalkombatservice.controller.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,13 +17,14 @@ import java.util.List;
 @RequestMapping("/")
 public class MortalKombatControllerImpl {
 
+    @Autowired
+    private  RestTemplate restTemplate;
+
     @GetMapping("chooseyourdestiny")
     public List<Path> getFightingPaths() {
 
-        RestTemplate restTemplate = new RestTemplate();
         FighterInfoVO fighterInfo = restTemplate.getForObject("http://localhost:8082/getFighterInfo/1", FighterInfoVO.class);
         ChanceVO chance = restTemplate.getForObject("http://localhost:8083/getchance", ChanceVO.class);
-
 
         return Collections.singletonList(new Path(fighterInfo.getId(), fighterInfo.getName(), chance.getWinChance()));
     }
