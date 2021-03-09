bef6b6867cd675c13ba52ee6b423496c0497fbba
RUSLAN SIMAKOV <ua667766706@gmail.com>
2021-03-09 23:41:22 +0200

    Refactoring with webclientBuilder

--- mortalkombatservice/src/main/java/ruslan/simakov/mortalkombatservice/controller/impl/MortalKombatControllerImpl.java
+++ mortalkombatservice/src/main/java/ruslan/simakov/mortalkombatservice/controller/impl/MortalKombatControllerImpl.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import ruslan.simakov.mortalkombatservice.model.ChanceVO;
 import ruslan.simakov.mortalkombatservice.model.FighterInfoVO;
 import ruslan.simakov.mortalkombatservice.model.Path;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -18,13 +18,23 @@ import java.util.List;
 public class MortalKombatControllerImpl {
 
     @Autowired
-    private  RestTemplate restTemplate;
+    private RestTemplate restTemplate;
+    @Autowired
+    private WebClient.Builder webClientBuilder;
 
     @GetMapping("chooseyourdestiny")
     public List<Path> getFightingPaths() {
 
-        FighterInfoVO fighterInfo = restTemplate.getForObject("http://localhost:8082/getFighterInfo/1", FighterInfoVO.class);
-        ChanceVO chance = restTemplate.getForObject("http://localhost:8083/getchance", ChanceVO.class);
+        ChanceVO chance = webClientBuilder.build()
+                .get()
+                .uri("http://localhost:8083/getchance")
+                .retrieve()
+                .bodyToMono(ChanceVO.class)
+                .block();
+
+        FighterInfoVO fighterInfo = restTemplate.getForObject(
+                "http://localhost:8082/getFighterInfo/1",
+                FighterInfoVO.class);
 
         return Collections.singletonList(new Path(fighterInfo.getId(), fighterInfo.getName(), chance.getWinChance()));
     }
